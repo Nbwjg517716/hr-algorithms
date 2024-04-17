63eab0c5b84a4644e206267798e2da2ccb7477cb
nicolas_jesus <130939378+Nbwjg517716@users.noreply.github.com>
2024-04-16 21:07:55 -0300

    Create web_scraping.php
    estudo de php

---
+++ web_scraping.php
@@ -0,0 +1,23 @@
+<?php
+$html = file_get_contents("https://proceedings.science/papers-published");
+libxml_use_internal_errors(true);
+$documento = new DOMDocument();
+$documento->loadHTML($html);
+
+$domNodelist = $documento->getElementsByTagName("a");
+$linklist = [];
+
+foreach ($domNodelist as $link) {
+    $href = $link->getAttribute("href");
+    if (!empty($href)) {
+        $linklist[] = $href;
+    }
+}
+
+// Abrir arquivo
+$arquivo = fopen('file.csv', 'w');
+// Escrever conteúdo
+foreach ($linklist as $link) {
+    fputcsv($arquivo, [$link]);
+}
+fclose($arquivo);
